18caea8ec9d61eb834c44d32ab49a8239fdee57d
ysawyers <yondenj.sawyers09@gmail.com>
2023-06-01 18:10:57 -0400

    push

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig, loadEnv } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import solidSvg from "vite-plugin-solid-svg";
-import pluginRewriteAll from "vite-plugin-rewrite-all";
 
 export default ({ mode }: { mode: any }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-    plugins: [solidPlugin(), solidSvg(), pluginRewriteAll()],
+    plugins: [solidPlugin(), solidSvg()],
     server: {
-      host: process.env.VITE_HOST,
-      port: parseInt(process.env.VITE_PORT!),
+      host: "0.0.0.0",
+      port: 80,
     },
     build: {
       target: "esnext",
