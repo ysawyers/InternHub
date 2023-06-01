521dbdb344a45ed70681af2feafbc78bbb477efa
ysawyers <yondenj.sawyers09@gmail.com>
2023-06-01 11:07:01 -0400

    updated vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,8 +8,8 @@ export default ({ mode }: { mode: any }) => {
   return defineConfig({
     plugins: [solidPlugin(), solidSvg()],
     server: {
-      host: process.env.VITE_HOST,
-      port: parseInt(process.env.VITE_PORT!),
+      host: "0.0.0.0",
+      port: 8080,
     },
     build: {
       target: "esnext",
