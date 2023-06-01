694964db3587ed28b66a52b222d37f991c7f7ccc
ysawyers <yondenj.sawyers09@gmail.com>
2023-06-01 03:32:50 -0400

    updated vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import solidSvg from "vite-plugin-solid-svg";
 
-export default defineConfig({
-  plugins: [solidPlugin(), solidSvg()],
-  server: {
-    host: "127.0.0.1",
-    port: 3000,
-  },
-  build: {
-    target: "esnext",
-  },
-});
+export default ({ mode }: { mode: any }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [solidPlugin(), solidSvg()],
+    server: {
+      host: process.env.VITE_HOST,
+      port: parseInt(process.env.VITE_PORT!),
+    },
+    build: {
+      target: "esnext",
+    },
+  });
+};
